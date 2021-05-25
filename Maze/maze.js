dcfae369179f250e28a85a622d4c8b89ac41a9e7
allenn159 <allenn159@gmail.com>
2021-05-24 20:22:48 -0700

    added demo and made changes to master

--- Maze/maze.js
+++ Maze/maze.js
@@ -1,7 +1,7 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
-  Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const cells = 5;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -11,7 +11,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: false,
+    wireframes: true,
     width,
     height,
   },
@@ -20,47 +20,38 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {
+  Bodies.rectangle(width / 2, 0, width, 40, {
     isStatic: true,
   }),
-  Bodies.rectangle(400, 600, 800, 40, {
+  Bodies.rectangle(width / 2, height, width, 40, {
     isStatic: true,
   }),
-  Bodies.rectangle(0, 300, 40, 600, {
+  Bodies.rectangle(0, height / 2, 40, height, {
     isStatic: true,
   }),
-  Bodies.rectangle(800, 300, 40, 600, {
+  Bodies.rectangle(width, height / 2, 40, height, {
     isStatic: true,
   }),
 ];
 
 World.add(world, walls);
 
-// Random Shapes.
+// Maze generation
 
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: "red",
-        },
-      })
-    );
-  }
-}
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+console.log(grid);
+console.log(verticals);
+console.log(horizontals);
